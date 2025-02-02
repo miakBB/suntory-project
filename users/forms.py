d53708f382a52f322811a7850c1fcd1779750f08
Ibuki Makino <ibuki.makino0328@gmail.com>
2025-02-02 19:07:52 +0900

    細かな修正

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class UserRegistrationForm(UserCreationForm):
     email=forms.EmailField(required=True) #メールアドレスを必須にする文
 
     class Meta:
-        model=User
-        fields=('username','email','password1','password2')
+        model=get_user_model()
+        #fields=('username','email','password1','password2')
+        fields = ('username', 'email', 'password1', 'password2', 'birth_date', 'gender', 'weight')
