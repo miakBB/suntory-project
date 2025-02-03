7831e63e19d9ea91824833ed7dd401aff9a3024c
Ibuki Makino <ibuki.makino0328@gmail.com>
2025-02-04 00:53:52 +0900

    UIの改善、新規登録にエラーチェック実装

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import register,mypage
-from django.contrib.auth import views as auth_views
+from .views import register, mypage, CustomLoginView
 
 
 urlpatterns=[
     path('register/',register,name='register'),
-    path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('mypage/',mypage,name='mypage'),
 ] 
--- users/views.py
+++ users/views.py
@@ -1,18 +1,43 @@
-from django.shortcuts import render,redirect
-from .forms import UserRegistrationForm,UserUpdateForm
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm, UserUpdateForm, UserLoginForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 
+#カスタムログインビュー(メールアドレス)
+class CustomLoginView(LoginView):
+    authentication_form = UserLoginForm
+    template_name = 'login.html'
+
+#ユーザー登録
+"""
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request,user) #登録後にログイン
-            return redirect('login_main')#メインログインへリダイレクト
+            user = form.save(commit=False)  # 一旦保存せずにデータを取得
+            user.gender = int(form.cleaned_data['gender'])  # 性別を整数に変換
+            user.save()  # 保存
+            login(request, user)  # 登録後にログイン
+            return redirect('login_main')  # メインログインへリダイレクト
     else:
         form = UserRegistrationForm()
-    return render(request,'register.html',{'form':form}) 
+    return render(request, 'register.html', {'form': form})
+    """
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.gender = int(form.cleaned_data['gender'])  # 性別を整数に変換
+            user.save()
+            login(request, user)
+            return redirect('login_main')  # メインログインへリダイレクト
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'register.html') 
+
 
 @login_required
 def mypage(request):
