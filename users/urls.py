c9a79140aaa6b70883ef4c1a1d051b39bf4ca332
iwase151122 <iwase.f.24kdgn@gmail.com>
2025-02-03 12:33:27 +0900

    feat:ユーザー情報更新機能追加

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register
+from .views import register,mypage
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns=[
     path('register/',register,name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+    path('mypage/',mypage,name='mypage'),
 ] 
